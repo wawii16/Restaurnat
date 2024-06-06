ebea4fdef950a25709a432b74f0c61b1fed42279
Dwi Wahyu <dwiie.w.maulana@gmail.com>
2024-06-06 15:32:02 +0700

    second commit

--- webpack.common.js
+++ webpack.common.js
@@ -107,6 +107,13 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
-  ],
+    process.env.NODE_ENV === 'production' && new BundleAnalyzerPlugin({
+
+      analyzerMode: 'static',
+
+      openAnalyzer: false,
+
+    }),
+
+  ].filter(Boolean),
 };
